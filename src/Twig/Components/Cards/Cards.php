fd98f68d1246ecbf288482118152258322c8e2ab
mikeheul <mickael.murmann@gmail.com>
2024-01-29 16:06:48 +0100

    live component try

--- src/Twig/Components/Cards/Cards.php
+++ src/Twig/Components/Cards/Cards.php
@@ -2,10 +2,14 @@
 
 namespace App\Twig\Components;
 
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent(exposePublicProps: false)]
+// #[AsTwigComponent(exposePublicProps: false)]
+#[AsLiveComponent]
 class Cards
 {
+    use DefaultActionTrait;
     public array $cards;
 }
